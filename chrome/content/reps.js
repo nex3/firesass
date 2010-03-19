285a3af00b811e7643945c406604895d6f18396a
Nathan Weizenbaum <nex342@gmail.com>
2010-03-19 03:49:24 -0700

    Actually add code to grab the Sass debug info.

---
+++ chrome/content/reps.js
@@ -0,0 +1,69 @@
+FBL.ns(function() { with (FBL) {
+    var sl = Firebug.getRep(new FBL.SourceLink());
+
+    function cacheSassDebugInfo(sourceLink) {
+        if (sourceLink.type != "css" || sourceLink.sassDebugInfo) {
+            sourceLink.sassDebugInfo = {};
+            return;
+        }
+
+        var rules = sourceLink.object.parentStyleSheet.cssRules;
+        for(var i=0; i<rules.length-1; i++)
+        {
+            var styleRule = rules[i+1];
+            if (styleRule.type != CSSRule.STYLE_RULE) continue;
+            styleRule.sassDebugInfo = {};
+
+            var mediaRule = rules[i];
+            if (mediaRule.type != CSSRule.MEDIA_RULE) continue;
+
+            if (mediaRule.media.mediaText != "-sass-debug-info") continue;
+
+            for (var j=0; j<mediaRule.cssRules.length; j++)
+            {
+                styleRule.sassDebugInfo[mediaRule.cssRules[j].selectorText] =
+                    mediaRule.cssRules[j].style.getPropertyValue("font-family");
+            }
+        }
+
+        sourceLink.sassDebugInfo = sourceLink.object.sassDebugInfo || {};
+        return;
+    }
+
+    sl.getSourceLinkTitle = function(sourceLink)
+    {
+        if (!sourceLink)
+            return "";
+
+        cacheSassDebugInfo(sourceLink);
+
+        try
+        {
+            var fileName = getFileName(sourceLink.sassDebugInfo["filename"] || sourceLink.href);
+            fileName = decodeURIComponent(fileName);
+            fileName = cropString(fileName, 17);
+        }
+        catch(exc)
+        {
+        }
+
+        return $STRF("Line", [fileName, sourceLink.sassDebugInfo["line"] || sourceLink.line]);
+    };
+
+    sl.copyLink = function(sourceLink)
+    {
+        var sassFilename = sourceLink.sassDebugInfo["filename"];
+        if (sassFilename)
+        {
+            var url = splitURLTrue(sassFilename);
+            copyToClipboard(url.path + "/" + url.name);
+        }
+        else
+            copyToClipboard(sourceLink.href);
+    };
+
+    sl.getTooltip = function(sourceLink)
+    {
+        return decodeURI(sourceLink.sassDebugInfo["filename"] || sourceLink.href);
+    };
+}});
